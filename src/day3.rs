1efbe9d52527deea85d39e1976dc623d51d1202f
Michael Gajewski <gajewski.mike@gmail.com>
2023-12-03 20:28:02 +0000

    Part 1 done - some work on part 2

---
+++ src/day3.rs
@@ -0,0 +1,172 @@
+#[path = "./util.rs"] mod util;
+
+use std::{collections::HashSet};
+use regex::Regex;
+
+pub fn run() {
+    let day3 = Day3::new();
+    //day3.part_1();
+    day3.part_2();
+}
+
+pub struct Day3 {
+    // Empty.
+}
+
+impl Day3 {
+    pub fn new() -> Day3 {
+        Day3{}
+    }
+
+    pub fn part_1(self) {
+        let output = util::read_file_to_lines("./inputs/day3.txt");
+
+        let mut i = 0;
+        let mut prev_line: String = String::from("");
+        let mut current_line: String = String::from("");
+        let mut next_line: String = String::from("");
+
+        let mut all_part_numbers: Vec<String> = Vec::new();
+
+        while i < output.len() {
+            prev_line = String::from(&current_line);
+            current_line = String::from(&output[i]);
+
+            if (i + 1) < output.len() {
+                next_line = String::from(&output[i + 1]);
+            }
+            else {
+                next_line = String::from("");
+            }
+
+            let mut part_numbers_prev = Day3::_find_part_numbers_in_lines(&current_line, &prev_line);
+            let mut part_numbers_current = Day3::_find_part_numbers_in_lines(&current_line, &current_line);
+            let mut part_numbers_next = Day3::_find_part_numbers_in_lines(&current_line, &next_line);
+
+            let mut all_part_numbers_this_iter: Vec<(usize, String)> = Vec::new();
+            all_part_numbers_this_iter.extend(part_numbers_prev.iter().cloned());
+            all_part_numbers_this_iter.extend(part_numbers_current.iter().cloned());
+            all_part_numbers_this_iter.extend(part_numbers_next.iter().cloned());
+
+
+            all_part_numbers_this_iter.dedup_by(|a, b| a.0 == b.0 && a.1 == b.1);
+            all_part_numbers.extend(all_part_numbers_this_iter.iter().cloned().map(|x|x.1));
+
+            i += 1;
+        }
+
+        let part_number_sum: i32 = all_part_numbers
+            .iter()
+            .map(|x|x.parse::<i32>().unwrap())
+            .sum();
+
+        println!("All Part Numbers: {}", all_part_numbers.join(","));
+        println!("Part number sum: {}", part_number_sum);
+
+    }
+
+    pub fn part_2(self) {
+        let output = util::read_file_to_lines("./inputs/day3.txt");
+
+        let mut i = 0;
+        let mut prev_line: String = String::from("");
+        let mut current_line: String = String::from("");
+        let mut next_line: String = String::from("");
+
+        let mut all_part_numbers: Vec<String> = Vec::new();
+
+        while i < output.len() {
+            prev_line = String::from(&current_line);
+            current_line = String::from(&output[i]);
+
+            if (i + 1) < output.len() {
+                next_line = String::from(&output[i + 1]);
+            }
+            else {
+                next_line = String::from("");
+            }
+
+            
+
+            i += 1;
+        }
+
+        let part_number_sum: i32 = all_part_numbers
+            .iter()
+            .map(|x|x.parse::<i32>().unwrap())
+            .sum();
+
+        println!("All Part Numbers: {}", all_part_numbers.join(","));
+        println!("Part number sum: {}", part_number_sum);
+
+    }
+
+    fn _find_numbers(line: &String) -> Vec<(usize, String)> {
+        let number_re = Regex::new(r"(\d+)+").unwrap();
+
+        return number_re
+            .find_iter(&line)
+            .filter_map(|number| Some((number.start(), number.as_str().parse().ok().unwrap())))
+            .collect::<Vec<(usize, String)>>();
+    }
+
+    fn _find_gear_symbol_positions(haystack: &String) -> Vec<usize> {
+        let mut indices = Vec::new();
+
+        for (index, c) in haystack.char_indices() {
+            if c == '*' {
+                indices.push(index);
+            }
+        }
+
+        return indices;
+    }
+
+    fn _find_symbol_positions(haystack: &String) -> Vec<usize> {
+        let mut indices = Vec::new();
+
+        for (index, c) in haystack.char_indices() {
+            if !c.is_alphanumeric() && c != '.' {
+                indices.push(index);
+            }
+        }
+
+        return indices;
+    }
+
+    fn _combine_part_number_vectors(v: &Vec<String>, v2: &Vec<String>, remove_dupes: bool) -> Vec<String> {
+        let mut combined = v.clone();
+        combined.extend(v2.clone());
+
+        if remove_dupes {
+            return combined
+                .into_iter()
+                .collect::<HashSet<String>>()
+                .into_iter()
+                .collect();
+        }
+
+        return combined;
+    }
+
+    fn _find_part_numbers_in_lines(target_line: &String, compare_line: &String) -> Vec<(usize, String)> {
+        let symbol_positions = Day3::_find_symbol_positions(&compare_line);
+        let numbers = Day3::_find_numbers(&target_line);
+        let mut part_numbers = Vec::new();
+
+        for number in numbers.iter() {
+
+            let number_string_end_pos = number.0 + (number.1.len() - 1);
+            let start_check = if number.0 > 0 { number.0 - 1} else { 0 };
+            let end_check = number_string_end_pos + 1;
+
+            for symbol_pos in symbol_positions.iter() {
+                if symbol_pos >= &start_check && symbol_pos <= &end_check {
+                    part_numbers.push(number.clone());
+                }
+            }
+        }
+
+        return part_numbers;
+    }
+}
