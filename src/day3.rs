86f724c31681a70eee71376ef4feb9aec423bc3a
Michael Gajewski <gajewski.mike@gmail.com>
2023-12-03 22:20:22 +0000

    day 3 sucked

--- src/day3.rs
+++ src/day3.rs
@@ -73,7 +73,7 @@ impl Day3 {
         let mut current_line: String = String::from("");
         let mut next_line: String = String::from("");
 
-        let mut all_part_numbers: Vec<String> = Vec::new();
+        let mut gear_ratios: Vec<i32> = Vec::new();
 
         while i < output.len() {
             prev_line = String::from(&current_line);
@@ -86,18 +86,36 @@ impl Day3 {
                 next_line = String::from("");
             }
 
-            
+            let gear_number_sets = Day3::_find_gears(&current_line, &prev_line, &next_line);
+
+            println!("");
+            println!("--Line {}--", i + 1);
+
+            for set in gear_number_sets {
+                print!("[ {} ] -> ", set.iter().map(|x|x.1.to_string()).collect::<Vec<String>>().join(","));
+                
+                let mut p = 1;
+                for number in set {
+                    p *= number.1.parse::<i32>().unwrap();
+                }
+
+                print!("{}", p);
+                println!("");
+
+                gear_ratios.push(p);
+            }
+
+            println!("--END Line {}--", i + 1);
+
+            println!("");
 
             i += 1;
         }
 
-        let part_number_sum: i32 = all_part_numbers
-            .iter()
-            .map(|x|x.parse::<i32>().unwrap())
-            .sum();
-
-        println!("All Part Numbers: {}", all_part_numbers.join(","));
-        println!("Part number sum: {}", part_number_sum);
+        println!("");
+        let total_sum: i32 = gear_ratios.iter().sum();
+        println!("Sum of gear ratios: {}", total_sum);
+        println!("");
 
     }
 
@@ -132,6 +150,67 @@ impl Day3 {
         }
 
         return indices;
+    }
+
+    fn _find_gears(current_line: &String, prev_line: &String, next_line: &String) -> Vec<Vec<(usize, String)>> {
+        let gear_symbol_positions = Day3::_find_gear_symbol_positions(current_line);
+        if gear_symbol_positions.len() == 0 {
+            return vec![vec![(0, String::from("0")), (0, String::from("0"))]];
+        }
+
+        let mut gear_number_sets: Vec<Vec<(usize, String)>> = Vec::new();
+
+        for gear_symbol_position in gear_symbol_positions {
+            let mut gear_numbers: Vec<(usize, String)> = Vec::new();
+
+            // Previous line.
+            if prev_line.len() > 0 {
+                let prev_numbers = Day3::_find_numbers(&prev_line);
+                
+                for prev_number in prev_numbers {
+                    let number_string_end_pos = prev_number.0 + (prev_number.1.len() - 1);
+                    let start_check = if prev_number.0 > 0 { prev_number.0 - 1} else { 0 };
+                    let end_check = number_string_end_pos + 1;
+
+                    if gear_symbol_position >= start_check && gear_symbol_position <= end_check {
+                        gear_numbers.push(prev_number);
+                    }
+                }
+            }
+
+            // Current line.
+            let current_numbers = Day3::_find_numbers(&current_line);
+            for current_number in current_numbers {
+                let number_string_end_pos = current_number.0 + (current_number.1.len() - 1);
+                let start_check = if current_number.0 > 0 { current_number.0 - 1} else { 0 };
+                let end_check = number_string_end_pos + 1;
+
+                if gear_symbol_position >= start_check && gear_symbol_position <= end_check {
+                    gear_numbers.push(current_number);
+                }
+            }
+
+            // Next line.
+            if next_line.len() > 0 {
+                let next_numbers = Day3::_find_numbers(&next_line);
+                for next_number in next_numbers {
+                    let number_string_end_pos = next_number.0 + (next_number.1.len() - 1);
+                    let start_check = if next_number.0 > 0 { next_number.0 - 1} else { 0 };
+                    let end_check = number_string_end_pos + 1;
+
+                    if gear_symbol_position >= start_check && gear_symbol_position <= end_check {
+                        gear_numbers.push(next_number);
+                    }
+                }
+            }
+
+            if gear_numbers.len() == 2 {
+                gear_number_sets.push(gear_numbers);
+            }
+        }
+
+        return gear_number_sets;
+
     }
 
     fn _combine_part_number_vectors(v: &Vec<String>, v2: &Vec<String>, remove_dupes: bool) -> Vec<String> {
